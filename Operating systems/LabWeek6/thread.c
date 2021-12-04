0c8ecac5beba6718b04b573e71b659a452603145
Pulkit Mahajan <2019ucs0073@iitjammu.ac.in>
2021-12-05 00:52:26 +0530

    getting a return value from thread

--- Operating systems/LabWeek6/thread.c
+++ Operating systems/LabWeek6/thread.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
-
+#include <time.h>
 
 int x = 0 ;
 void* routine0()
@@ -47,15 +47,68 @@ void* routine4()
 	}
 }
 
+void* rollDice()
+{
+	int value = (rand()%6)+1;
+	int* result = malloc(sizeof(int));
+	*result = value;
+	return (void*)result;
+}
+
 int main(int argc, char* argv[])
 {
-	pthread_t t1, t2;
-	pthread_mutex_init(&mutex, NULL);
-	pthread_create(&t1, NULL, &routine4, NULL);
-	pthread_create(&t2, NULL, &routine4, NULL);
-	pthread_join(t1, NULL);
-	pthread_join(t2, NULL);
-	pthread_mutex_destroy(&mutex);
-	printf("Number of mails: %d\n",mails);
+	pthread_t t;
+	srand(time(NULL));
+	int **res; 
+	if(pthread_create(&t, NULL, &rollDice, NULL)){
+		perror("Error in creating a thread\n");
+		return 1;
+	}
+	if(pthread_join(t, (void**)res)){
+		perror("Error in joining back thread\n");
+		return 2;
+	}
+
+	printf("After rolling dice: %d\n",**res);
+	free(*res);
+	
+
+
+
+	// pthread_t t1, t2;
+	// pthread_mutex_init(&mutex, NULL);
+	// pthread_create(&t1, NULL, &routine4, NULL);
+	// pthread_create(&t2, NULL, &routine4, NULL);
+	// pthread_join(t1, NULL);
+	// pthread_join(t2, NULL);
+	// pthread_mutex_destroy(&mutex);
+	// printf("Number of mails: %d\n",mails);
+
+	// pthread_t th[10];
+	// pthread_mutex_init(&mutex, NULL);
+	
+	// for(int i = 0 ; i < 10 ;i++)
+	// {
+	// 	if(pthread_create(&th[i], NULL, &routine4, NULL)){
+	// 		perror("failed to create thread");
+	// 		return i+1;
+	// 	}
+	// }
+	// for(int i = 0 ; i < 10 ; i++)
+	// {
+	// 	if(pthread_join(th[i], NULL)){
+	// 		perror("failed to join thread\n");
+	// 		return i+1+10;
+	// 	}
+	// }
+	
+	// pthread_mutex_destroy(&mutex);
+	// printf("Number of mails: %d\n", mails);
+
+
+
 	return 0;
 }
+
+
+//race condititon can only occur on multicore processors
